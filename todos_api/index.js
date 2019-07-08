823f598704c3df4558d2e02c8971db4b2b753bdf
Krisztian Holub <kholub1989@gmail.com>
2019-07-08 15:10:09 -0400

    add static files and starter css

--- todos_api/index.js
+++ todos_api/index.js
@@ -8,14 +8,15 @@ var todoRoutes = require('./routes/todos');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
-
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/views'));
 
 app.get('/', function(req, res){
-   res.send("Hello forom the root route!")
+   res.sendFile('index.html');
 })
 
 app.use('/api/todos', todoRoutes);
 
 app.listen(3000, function(){
-    console.log("APP IS RUNNING ON PORT");
+    console.log("APP IS RUNNING ON PORT 3000");
 })
